54397061b0def033d80055c61ed97f05217979eb
Gustav Kjellberg <gustavkjellberg@Gustavs-MacBook-Pro.local>
2020-04-24 18:15:24 +0200

    Created plots for the first subtask and made an interactive file

--- Assignment-2-FeatureExtraction/Assignment2_updated/testReadAndGetFeats.m
+++ Assignment-2-FeatureExtraction/Assignment2_updated/testReadAndGetFeats.m
@@ -1,5 +1,9 @@
 clear;
 
-[Y, FS] = audioread('Songs/melody_1.wav');
+[Y1, FS1] = audioread('Songs/melody_1.wav');
+[Y2, FS2] = audioread('Songs/melody_2.wav');
+[Y3, FS3] = audioread('Songs/melody_3.wav');
 
-frIseqT = GetMusicFeatures(Y,FS,0.03);
+frIseqT1 = GetMusicFeatures(Y1,FS1,0.03);
+frIseqT2 = GetMusicFeatures(Y2,FS2,0.03);
+frIseqT3 = GetMusicFeatures(Y3,FS3,0.03);
