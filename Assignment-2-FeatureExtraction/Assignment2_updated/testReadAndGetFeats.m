3ab5d583887dabe9151864be440ffacc7f903d89
abbenteprizer <abbenteprizer@gmail.com>
2020-04-26 16:48:35 +0200

    some pictures and figures

--- Assignment-2-FeatureExtraction/Assignment2_updated/testReadAndGetFeats.m
+++ Assignment-2-FeatureExtraction/Assignment2_updated/testReadAndGetFeats.m
@@ -2,4 +2,32 @@ clear;
 
 [Y, FS] = audioread('Songs/melody_1.wav');
 
-frIseqT = GetMusicFeatures(Y,FS,0.03);
+%plot(Y) % plots audio waveform
+
+frIseqT = GetMusicFeatures(Y,FS,0.03); % extract all fourier data
+
+%size(frIseqT(1,:));
+figure('Name', 'Plots')
+subplot(2,2,1)
+%plot(normalize(frIseqT(1,:),'range'))
+plot(log(frIseqT(1,:)))
+title('Frequency')
+hold on
+frIseqT(1,:);
+
+%figure('Name', 'Correlation')
+subplot(2,2,2)
+plot(normalize(frIseqT(2,:),'range'))
+title('Correlation')
+
+subplot(2,2,3)
+%figure('Name', 'Intensity')
+plot(normalize(frIseqT(3,:),'range'))
+title('Intensity')
+hold off
+%basing your featureson the logarithm of the pitch track
+
+%distinctsemitones allows devising a discrete representation of the sound, if you like.
+
+%feature extractor where the output remains largely unchanged ifthe entire 
+%input is transposed up or down by an arbitrary amount
